bcd8046fabc97d4125574e46d55b7d96ac27a81d
Juan Manuel <mac.jmanuel@gmail.com>
2024-08-20 23:26:29 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mtech_ionic',
+  webDir: 'www'
+};
+
+export default config;
